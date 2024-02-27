0e8c20d543b679d3d51e935438e0f3e11958d1d4
chenp <chenpeng@copote.com>
2024-02-27 22:07:01 +0800

    升级v3

--- background.js
+++ background.js
@@ -1,39 +1,25 @@
-let isEnabled = true
-chrome.webRequest.onHeadersReceived.addListener(
-    function(details) {
-        var responseHeaders = details.responseHeaders;
-        if (isEnabled ){
-        responseHeaders.push({ name: 'Access-Control-Allow-Origin', value: '*' });
-        responseHeaders.push({ name: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' });
-        responseHeaders.push({ name: 'Access-Control-Allow-Credentials', value: 'true' });
-        }
-        var b = true;
-        for (var i = 0; i < responseHeaders.length; i++) {
-            if (responseHeaders[i].name.toLowerCase() === 'access-control-allow-headers') {
-                responseHeaders[i].value += ', x-mbx-apikey';
-                b = false
-            }
-        }
-        if (b){
-            responseHeaders.push({ name: 'Access-Control-Allow-Headers', value: 'x-mbx-apikey' });
-        }
-        return { responseHeaders: responseHeaders };
-    },
-    { urls: ['<all_urls>'] },
-    ['blocking', 'responseHeaders']
-);
-
+let isEnabled = false
 
 // background.js
-chrome.browserAction.onClicked.addListener(function(tab) {
-    if (isEnabled) {
+chrome.action.onClicked.addListener((tab) => {
+  if (isEnabled) {
         isEnabled = false
         // 如果当前图标为 "open"，则切换为 "close" 图标
-        chrome.browserAction.setIcon({ path: "close.png" });
-
+        chrome.action.setIcon({ path: "close.png" });
+		chrome.declarativeNetRequest.updateEnabledRulesets({
+		  disableRulesetIds: ["ruleset"],
+		}, () => {
+		  console.log("规则已禁用");
+		});
     } else {
         isEnabled = true
         // 如果当前图标为 "close"，则切换为 "open" 图标
-        chrome.browserAction.setIcon({ path: "open.png" });
+        chrome.action.setIcon({ path: "open.png" });
+		chrome.declarativeNetRequest.updateEnabledRulesets({
+		  enableRulesetIds: ["ruleset"],
+		}, () => {
+		  console.log("规则已启用");
+		});
+
     }
-});
+})
